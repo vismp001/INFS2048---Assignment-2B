a463fd40059decd34c58b8c7ddb0cea8d03a7f7d
vismp001 <vismp001@mymail.unisa.edu.au>
2023-03-13 18:22:37 +1030

    Added test_FileAccess.py    + Added fixtures demo    + Added not for limitations of testing

---
+++ test_FileAccess.py
@@ -0,0 +1,13 @@
+import pytest
+from word_statistics_app import *
+
+@pytest.fixture()
+def setupTXTOutput() -> Formatting:
+    return "File file1.txt contains 12 words. Frequent words are: a (2), This (1), is (1), test (1)"
+
+def setupCSVOutput() -> Formatting:
+    return "file1.txt,12,a,2,This,1,is,1,test,1"
+
+# Cannot test import or export functionalities without modifying external environment
+# Cannot use Mock due to limitations of assesment piece.
+#    - Tests that would run here otherwise, include test_importSuccess, test_importIsString and test_exportSuccess
