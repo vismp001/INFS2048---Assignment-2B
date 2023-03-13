d12035a953184aa20ec0b417a29f2cd228695f93
vismp001 <vismp001@mymail.unisa.edu.au>
2023-03-13 18:09:59 +1030

    Update word_statistics_app.py + Added generateOutput() method + Added getFormatTypeFromExtension() method * Updates arguments in generateOutput() to include fileName:str

--- test_Formatting.py
+++ test_Formatting.py
@@ -25,12 +25,28 @@ def test_interperetDescriptorsIsString(setupDescriptors):
 def test_interpretDescriptorExpectedOutputCSV(setupDescriptors):
     CSVFormatter:FormatType = CSVFormat()
     result:str = CSVFormatter.interpret('file1.txt', setupDescriptors)
-    print(result)
     assert result == "file1.txt,12,a,2,This,1,is,1,test,1"
 
 def test_interpretDescriptorExpectedOutputTXT(setupDescriptors):
     CSVFormatter:FormatType = TXTFormat()
     result:str = CSVFormatter.interpret('file1.txt', setupDescriptors)
-    print(result)
     assert result == "File file1.txt contains 12 words. Frequent words are: a (2), This (1), is (1), test (1)"
 
+def test_returnsCorrectFormatTypeTXT(setupFormatting:Formatting):
+    formatType:FormatType = setupFormatting.getFormatTypeFromExtension('txt')
+    assert isinstance(formatType, TXTFormat)
+
+def test_returnsCorrectFormatTypeCSV(setupFormatting:Formatting):
+    formatType:FormatType = setupFormatting.getFormatTypeFromExtension('csv')
+    assert isinstance(formatType, CSVFormat)
+
+def test_formattingGeneratesOutputString(setupFormatting:Formatting, setupDescriptors):
+    output:str = setupFormatting.generateOutput('file1.txt', setupDescriptors, TXTFormat())
+    assert isinstance(output, str)
+    #+ generateOutput(filename:str, descriptorInformation:list(DescriptorInfo), formatType:FormatType)
+
+# -
+# - extensionToFormatType : dict<string, FormatType>
+# -
+# + generateOutput(filename:str, descriptorInformation:list(DescriptorInfo), formatType:FormatType)
+# + getFormatTypeFromExtension(fileExtension:string) : FormatType
