b5930b3438f99bbb6f208534b5904793945e69e2
vismp001 <vismp001@mymail.unisa.edu.au>
2023-03-13 17:46:22 +1030

    Updated test_Formatting.py + Added setupToken fixture + Added setupFormatting fixture + Added setupDescriptors fixture
    + Added unit tests
      + Added test_interperetDescriptorsIsString
      + Added test_interpretDescriptorExpectedOutputCSV
      + test_interpretDescriptorExpectedOutputTXT

---
+++ test_Formatting.py
@@ -0,0 +1,36 @@
+import pytest
+from word_statistics_app import *
+
+def getTokens() -> list[str]:
+    return ['This', 'is', 'a', 'test', 'list', 'of', 'tokens', 'to', 'run', 'a', 'pytest', 'alongside']
+
+@pytest.fixture()
+def setupTokens():
+    return getTokens()
+
+@pytest.fixture()
+def setupFormatting() -> Formatting:
+    return Formatting()
+
+@pytest.fixture()
+def setupDescriptors() -> list[DescriptorInfo]:
+    descriptorInformation:list[DescriptorInfo] = [WordCount().describe(getTokens()), WordFrequency().describe(getTokens())]
+    return descriptorInformation
+
+def test_interperetDescriptorsIsString(setupDescriptors):
+    CSVFormatter:FormatType = CSVFormat()
+    result:str = CSVFormatter.interpret('file1.txt', setupDescriptors)
+    assert isinstance(result, str)
+
+def test_interpretDescriptorExpectedOutputCSV(setupDescriptors):
+    CSVFormatter:FormatType = CSVFormat()
+    result:str = CSVFormatter.interpret('file1.txt', setupDescriptors)
+    print(result)
+    assert result == "file1.txt,12,a,2,This,1,is,1,test,1"
+
+def test_interpretDescriptorExpectedOutputTXT(setupDescriptors):
+    CSVFormatter:FormatType = TXTFormat()
+    result:str = CSVFormatter.interpret('file1.txt', setupDescriptors)
+    print(result)
+    assert result == "File file1.txt contains 12 words. Frequent words are: a (2), This (1), is (1), test (1)"
+
