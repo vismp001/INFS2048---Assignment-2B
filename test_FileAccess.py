34b78ff52f568cc3271bc1fead36c15d600fb61a
vismp001 <vismp001@mymail.unisa.edu.au>
2023-03-15 23:07:31 +1030

    Updated test_FileAccess.py   + Added compile test

--- test_FileAccess.py
+++ test_FileAccess.py
@@ -5,8 +5,12 @@ from word_statistics_app import *
 def setupTXTOutput() -> Formatting:
     return "File file1.txt contains 12 words. Frequent words are: a (2), This (1), is (1), test (1)"
 
+@pytest.fixture
 def setupCSVOutput() -> Formatting:
     return "file1.txt,12,a,2,This,1,is,1,test,1"
+
+def fileAccessCompiles():
+    FileAccess()
 
 # Cannot test import or export functionalities without modifying external environment
 # Cannot use Mock due to limitations of assesment piece.
